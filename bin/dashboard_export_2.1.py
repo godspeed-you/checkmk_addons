de7c5ac69fe3d1453376a6c83dd26ea6866fc183
Marcel Arentz <marcel.arentz@tribe29.com>
2022-01-17 15:43:47 +0100

    updated exporter for upcoming version

--- bin/dashboard_export_2.1.py
+++ bin/dashboard_export_2.1.py
@@ -3,12 +3,18 @@
 """This tool converts user dashboards to local built in dashboards."""
 
 from os import environ
-from sys import argv
+from sys import argv, exit
+import subprocess
 from pathlib import Path
 import logging
 from pprint import pprint
 from ast import literal_eval
 import argparse
+
+INTERNAL_DASHBOARDS = [
+    'checkmk', 'checkmk_host', 'main', 'ntop_alerts', 'ntop_flows',
+    'ntop_top_talkers', 'problems', 'simple_problems', 'site'
+]
 
 
 class BaseVars():
@@ -22,15 +28,16 @@ class BaseVars():
         self.user = user
         self.board_name = board_name
         self.dashboards = dict()
+        self.local_path = str()
 
         if legacy:
             self.local_path = self.legacy_local_path
         else:
             # We cannot count on the existence of the sub directories... so we need to
             # create them, to be sure.
-            Path("{self.lib_local_path}/gui/plugins").mkdir(parents=True, exist_ok=True)
+            Path("{self.lib_local_path}/gui/plugins").mkdir(parents=True,
+                                                            exist_ok=True)
             self.local_path = f"{self.lib_local_path}/gui/plugins"
-
 
     def get_board_definition(self, name):
         """Return the definition of a given board"""
@@ -45,30 +52,44 @@ def parse_args(sysargv):
       -d / --dashboard
     """
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument(
-        "-u",
-        "--user",
-        required=True,
-        help="User from whom to extract the dashboards.")
+    parser.add_argument("-u",
+                        "--user",
+                        required=True,
+                        help="User from whom to extract the dashboards.")
     parser.add_argument(
         "-d",
         "--dashboard",
         required=False,
-        help="Dashboard to extract. If not provided, all dashboards will be extracted.")
+        help=
+        "Dashboard to extract. If not provided, all dashboards will be extracted."
+    )
     parser.add_argument(
         "-l",
         "--legacy",
         required=False,
         action="store_true",
         help="Create dashboards in the legacy local path instead of the new one"
-        )
+    )
+    parser.add_argument(
+        "-i",
+        "--include_builtin",
+        required=False,
+        action="store_true",
+        help="Also extract customized builtin dashboards to the local paths.")
+    parser.add_argument(
+        "-a",
+        "--restart_apache",
+        required=False,
+        action="store_true",
+        help=
+        "To see you new builtin dashboards, you need to restart the site apache. With this option, this is done automatically at the end."
+    )
     parser.add_argument(
         "-v",
         "--verbose",
         action="count",
         default=0,
-        help="Add debug output. Use several times for more output."
-        )
+        help="Add debug output. Use several times for more output.")
     return parser.parse_args(sysargv)
 
 
@@ -83,7 +104,8 @@ def setup_logging(verbosity):
     else:
         logging.disable(logging.CRITICAL)
         lvl = logging.CRITICAL
-    logging.basicConfig(level=lvl, format="%(asctime)s %(levelname)s %(message)s")
+    logging.basicConfig(level=lvl,
+                        format="%(asctime)s %(levelname)s %(message)s")
 
 
 def get_header():
@@ -100,13 +122,15 @@ from cmk.gui.plugins.dashboard.utils import builtin_dashboards, GROW, MAX
 
 def write_definition(base):
     """Write a file with the dashboard definition into the local structure of the site"""
-    with open(f"{base.local_path}/{base.user}_dashboard_{base.board_name}.py", "w") as local_out:
+    with open(f"{base.local_path}/{base.user}_dashboard_{base.board_name}.py",
+              "w") as local_out:
         logging.info("Create '%s'", local_out.name)
 
         local_out.write(get_header())
         logging.debug("Write dashboard line")
         local_out.write(f"builtin_dashboards[\"{base.board_name}\"] = ")
         logging.debug("Write dashboard content")
+        # use pprint to write content. It's better readable later on
         pprint(base.get_board_definition(base.board_name), local_out)
 
 
@@ -117,23 +141,35 @@ def main():
     'custom_{dashboard_name}.py'
     """
     args = parse_args(argv[1:])
+    incl_builtin = args.include_builtin
     exporter = BaseVars(args.user, args.dashboard, legacy=args.legacy)
     setup_logging(args.verbose)
     logging.debug("Parsed arguments: %s", args)
 
-    custom_boards = Path(f'{exporter.path}/{exporter.user}/user_dashboards.mk').read_text()
-    custom_boards = custom_boards.replace('\n', '')
-    exporter.dashboards = literal_eval(custom_boards)
+    dashboard_file = Path(
+        f'{exporter.path}/{exporter.user}/user_dashboards.mk')
+    if dashboard_file.exists():
+        custom_boards = dashboard_file.read_text()
+        custom_boards = custom_boards.replace('\n', '')
+        exporter.dashboards = literal_eval(custom_boards)
+    else:
+        exit("No customized dashboards found. Nothing to do.")
 
     logging.debug("Starting")
-    if exporter.board_name:
+    if exporter.board_name and incl_builtin:
         logging.info("Processing %s", exporter.board_name)
         write_definition(exporter)
     else:
         for board in exporter.dashboards:
+            if board in INTERNAL_DASHBOARDS and not incl_builtin:
+                continue
             logging.info("Processing %s", board)
             exporter.board_name = board
             write_definition(exporter)
+
+    if args.restart_apache:
+        subprocess.Popen(('omd', 'restart', 'apache'),
+                         stdout=subprocess.PIPE).communicate()
 
 
 if __name__ == "__main__":
